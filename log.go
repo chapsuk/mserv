9d6f3d98de410b13816441a7641ac23d6e72b499
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-10-25 14:07:56 +0300

    Remove panic

--- log.go
+++ log.go
@@ -9,8 +9,6 @@ var log Logger = stdlog.New(os.Stderr, "", stdlog.LstdFlags)
 
 // Logger interface for package things
 type Logger interface {
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
 	Print(v ...interface{})
 	Printf(format string, v ...interface{})
 }
